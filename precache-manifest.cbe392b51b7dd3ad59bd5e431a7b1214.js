236154c16f2012f3f6dff3195b89244656ec9831
utdwwc <32002006+nrajesh99@users.noreply.github.com>
2020-01-10 07:32:25 -0600

    Updates

---
+++ precache-manifest.cbe392b51b7dd3ad59bd5e431a7b1214.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed27b8d3673c3fe32c8b85d45d51d0b",
+    "url": "/wwc-website/index.html"
+  },
+  {
+    "revision": "ff276b36fdb64a1f3591",
+    "url": "/wwc-website/static/css/main.832b1522.chunk.css"
+  },
+  {
+    "revision": "4fa40e494b9efd39c280",
+    "url": "/wwc-website/static/js/2.047cb118.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/wwc-website/static/js/2.047cb118.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff276b36fdb64a1f3591",
+    "url": "/wwc-website/static/js/main.d49c4010.chunk.js"
+  },
+  {
+    "revision": "1b8191ed7cecc68862cd",
+    "url": "/wwc-website/static/js/runtime-main.b2f7650d.js"
+  },
+  {
+    "revision": "fd50fc7a73f1362440e18c213d19bac7",
+    "url": "/wwc-website/static/media/wwc-kickoff.fd50fc7a.jpg"
+  },
+  {
+    "revision": "28c1907a218302de8379ebb7c2541d33",
+    "url": "/wwc-website/static/media/wwc-team.28c1907a.jpg"
+  }
+]);
